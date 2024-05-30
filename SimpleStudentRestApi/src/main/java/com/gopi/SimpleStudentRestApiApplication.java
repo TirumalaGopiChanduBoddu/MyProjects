23702ab5e5831923c4127c463531a5945df1dc24
tirumala <tirumala.boddu82@gmail.com>
2024-05-30 23:23:46 +0530

    Simple Rest Api - Without SQL Data Base

---
+++ SimpleStudentRestApi/src/main/java/com/gopi/SimpleStudentRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStudentRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStudentRestApiApplication.class, args);
+	}
+
+}
---
+++ SimpleStudentRestApi/src/main/java/com/gopi/controller/StudentController.java
@@ -0,0 +1,77 @@
+package com.gopi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gopi.bean.Student;
+
+@RestController  // it is the combination of @Controller and @ResponseBody
+public class StudentController {
+
+	//http://localhost:8080/student
+	@GetMapping("/student")
+	public ResponseEntity<Student> getStudent() {
+		Student student = new  Student("Gopichandu" , 102 , "gopi@gmail.com" , "9989204279");;
+		return new ResponseEntity<>(student , HttpStatus.OK);
+	}
+	
+	//http://localhost:8080/students   -- it will return the list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		List<Student> list = new ArrayList<Student>();
+		list.add(new Student("Gopichandu" , 102 , "gopi@gmail.com" , "9989204279"));
+		list.add(new Student("Tirumala" , 102 , "tirumala@gmail.com" , "457845587"));
+		list.add(new Student("boddu" , 102 , "boddu@gmail.com" , "1455477788"));
+		return list;
+	}
+	
+	
+	@GetMapping("/student/{rno}")
+	public Student getStudentpathVarible(@PathVariable("rno") int studentRno ){
+		return new Student("boddu" , studentRno , "boddu@gmail.com" , "1455477788");
+		
+	}
+	
+	//http://localhost:8080/student/query?name=Gopi&rno=1222&emailId=gopiii@gmaiul.com&mobileNo=02155555
+	@GetMapping("/student/query")
+	public Student getStudentQueryParam(@RequestParam String name  , @RequestParam int rno , @RequestParam String emailId, @RequestParam String mobileNo){
+		return new Student(name , rno , emailId ,mobileNo);
+	}
+	
+	
+	//http://localhost:8080/student/create
+	@PostMapping("/student/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Student createStudent(@RequestBody Student student) {
+		System.out.println(student);
+		return student;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
