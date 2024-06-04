0d24487ec73ceea4243a60e1d3ecae600db081ee
tirumala <tirumala.boddu82@gmail.com>
2024-06-04 23:24:13 +0530

    One to one Bi-Directional Mapping

---
+++ OneToOne_Mapping_Bi/src/main/java/com/gopi/entity/Address.java
@@ -0,0 +1,76 @@
+package com.gopi.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="address_tbl")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="address_id")
+	private Long id;
+	private String street;
+	private String city;
+	private String state;
+	private String country;
+	private String pinCode;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="order_id" , referencedColumnName = "order_id")
+	private Order order;
+	
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPinCode() {
+		return pinCode;
+	}
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+	
+	
+}
